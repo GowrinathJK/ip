42796c626752f059693775c799eb5917c3a21c4c
GowrinathJK <e1591107@u.nus.edu>
2026-08-17 11:03:59 +0800

    Chanaged name and added greetings

---
+++ src/main/java/Vermithor.java
@@ -0,0 +1,11 @@
+public class Vermithor {
+    public static void main(String[] args) {
+        String banner = "__     __                  _ _   _                \n"
+                + "\\ \\   / /__ _ __ _ __ ___ (_) |_| |__   ___  _ __ \n"
+                + " \\ \\ / / _ \\ '__| '_ ` _ \\| | __| '_ \\ / _ \\| '__|\n"
+                + "  \\ V /  __/ |  | | | | | | | |_| | | | (_) | |   \n"
+                + "   \\_/ \\___|_|  |_| |_| |_|_|\\__|_| |_|\\___/|_|   \n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm VERMITHOR. \nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+    }
+}
