be22ac04674fd7ad569830b22b9a2dc94cb506cf
GowrinathJK <e1591107@u.nus.edu>
2026-08-17 14:40:32 +0800

    Added echo feature and exit on bye

--- src/main/java/Vermithor.java
+++ src/main/java/Vermithor.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Vermithor {
+
     public static void main(String[] args) {
-        String banner = "__     __                  _ _   _                \n"
-                + "\\ \\   / /__ _ __ _ __ ___ (_) |_| |__   ___  _ __ \n"
-                + " \\ \\ / / _ \\ '__| '_ ` _ \\| | __| '_ \\ / _ \\| '__|\n"
-                + "  \\ V /  __/ |  | | | | | | | |_| | | | (_) | |   \n"
-                + "   \\_/ \\___|_|  |_| |_| |_|_|\\__|_| |_|\\___/|_|   \n";
+        String banner = "__      __\n"
+                + "\\ \\    / /__ _ __ _ __ ___ (_) |_| |__   ___  _ __\n"
+                + " \\ \\  / / _ \\ '__| '_ ` _ \\| | __| '_ \\ / _ \\| '__|\n"
+                + "  \\ \\/ /  __/ |  | | | | | | | |_| | | | (_) | |\n"
+                + "   \\__/ \\___|_|  |_| |_| |_|_|\\__|_| |_|\\___/|_|\n";
+
         System.out.println(banner);
-        System.out.println("Hello! I'm VERMITHOR. \nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Vermithor.\nWhat can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
