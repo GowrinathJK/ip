f3b9659ff1bf23b76a7a9be93cbed207ad5db063
GowrinathJK <e1591107@u.nus.edu>
2026-08-17 15:06:35 +0800

    Add task status commands for Level-3

---
+++ src/main/java/Task.java
@@ -0,0 +1,38 @@
+/**
+ * Represents one task and whether it has been completed.
+ */
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description the task description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /** Marks this task as complete. */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /** Marks this task as incomplete. */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns this task in the format shown in Vermithor's task list.
+     *
+     * @return the status icon followed by the description
+     */
+    @Override
+    public String toString() {
+        String statusIcon = isDone ? "X" : " ";
+        return "[" + statusIcon + "] " + description;
+    }
+}
--- src/main/java/Vermithor.java
+++ src/main/java/Vermithor.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
+
+/**
+ * A command-line chatbot that records tasks and lets users update their status.
+ */
 public class Vermithor {
+
+    /**
+     * Starts Vermithor and processes commands until the user says goodbye.
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
         String banner = "__      __\n"
                 + "\\ \\    / /__ _ __ _ __ ___ (_) |_| |__   ___  _ __\n"
@@ -11,7 +21,7 @@ public class Vermithor {
         System.out.println("Hello! I'm Vermithor.\nWhat can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -23,11 +33,24 @@ public class Vermithor {
             }
 
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                Task task = tasks[taskNumber - 1];
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                Task task = tasks[taskNumber - 1];
+                task.markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
