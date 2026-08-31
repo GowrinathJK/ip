40ce2edd25cd066974113fd99fbc56e70896624f
GowrinathJK <e1591107@u.nus.edu>
2026-08-31 15:50:28 +0800

    Launch JavaFX GUI from executable JAR

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ BUILD_DIR="build/classes"
 rm -rf build
 mkdir -p "$BUILD_DIR"
 javac --release 25 -d "$BUILD_DIR" src/main/java/vermithor/*.java
-jar --create --file build/vermithor.jar --main-class vermithor.Vermithor -C "$BUILD_DIR" .
+jar --create --file build/vermithor.jar --main-class vermithor.Launcher -C "$BUILD_DIR" .
 echo "Created build/vermithor.jar"
